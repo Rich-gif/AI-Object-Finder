a24bf21fa392ecc6c04a4290009a5c5bb71769c2
Rich-gif <86768287+Rich-gif@users.noreply.github.com>
2022-11-01 20:58:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+video="";
+
+/*function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}*/
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status".innerHTML="Status : Detecting Objects")
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    video.loop();
+    video.speed(2);
+    video.volume(0);
+}
